0646fe9f78e4d5a4969c873a5815ebd9d51b7165
Mark Harman <mark.harman@ntlworld.com>
2012-05-12 23:36:31 +0100

    Add ASSERT_LOGGER.

--- game.h
+++ game.h
@@ -521,3 +521,40 @@ const bool LOGGING = true; // enable logging even for release builds, for now
 #ifndef LOG
 #define LOG if( !LOGGING ) ((void)0); else game_g->log
 #endif
+
+/** If test is not true:
+  *   In debug versions, writes the test, file and line to the log, and exits (via assert).
+  *   In release versions, we still write to the log file, but then continue.
+  */
+#ifndef ASSERT_LOGGER
+#ifdef _DEBUG
+
+#define ASSERT_LOGGER(test) {                          \
+        bool v = test;                                 \
+        if( v )                                        \
+                ((void)0);                             \
+        else {                                         \
+                LOG("ASSERTION FAILED:\n");            \
+                LOG("%s\n", #test);                    \
+                LOG("File: %s\n", __FILE__);           \
+                LOG("Line: %d\n", __LINE__);           \
+                assert(test);                          \
+        }                                              \
+}
+
+#else
+
+#define ASSERT_LOGGER(test) {                          \
+        bool v = test;                                 \
+        if( v )                                        \
+                ((void)0);                             \
+        else {                                         \
+                LOG("ASSERTION FAILED:\n");            \
+                LOG("%s\n", #test);                    \
+                LOG("File: %s\n", __FILE__);           \
+                LOG("Line: %d\n", __LINE__);           \
+        }                                              \
+}
+
+#endif
+#endif
