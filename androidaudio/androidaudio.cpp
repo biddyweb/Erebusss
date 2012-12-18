4d9105418a5cca0dcb08298c27d944bcda01f94f
Mark Harman <mark.harman@ntlworld.com>
2012-12-18 21:16:26 +0000

    Add AndroidAudio credit to .cpp files as well as .h files.

--- androidaudio/androidaudio.cpp
+++ androidaudio/androidaudio.cpp
@@ -1,3 +1,5 @@
+// modified from https://groups.google.com/forum/?fromgroups=#!topic/android-qt/rpPa_W6PF1Y , by Adam Pigg
+
 #include <QtGlobal> // need this to get Q_OS_ANDROID #define, which we need before we include anything else!
 
 #if defined(Q_OS_ANDROID)
