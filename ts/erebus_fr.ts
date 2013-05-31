5a92c3f8e41a9d3c07d7695fe511033f78c58b3a
Mark Harman <mark.harman@ntlworld.com>
2013-06-01 00:41:35 +0100

    Update to format in my Qt version.

--- ts/erebus_fr.ts
+++ ts/erebus_fr.ts
@@ -6,67 +6,67 @@
     <message>
         <location filename="../optionsgamestate.cpp" line="59"/>
         <source>Start game</source>
-        <translation type="finished">Lancer partie</translation>
+        <translation>Lancer partie</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="64"/>
         <source>Start playing a new game (Return)</source>
-        <translation type="finished">Lance une nouvelle partie (Entrée)</translation>
+        <translation>Lance une nouvelle partie (Entrée)</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="70"/>
         <source>Load game</source>
-        <translation type="finished">Charger jeux</translation>
+        <translation>Charger jeux</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="75"/>
         <source>Load a previously saved game (L)</source>
-        <translation type="finished">Charge une partie sauvegardée (L)</translation>
+        <translation>Charge une partie sauvegardée (L)</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="81"/>
         <source>Options</source>
-        <translation type="finished">Options</translation>
+        <translation>Options</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="86"/>
         <source>Configure various game options (O)</source>
-        <translation type="finished">Configuration du jeux</translation>
+        <translation>Configuration du jeux</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="94"/>
         <source>Quit game</source>
-        <translation type="finished">Quitter</translation>
+        <translation>Quitter</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="99"/>
         <source>Exit the game (Escape)</source>
-        <translation type="finished">Quitter le jeux (Echap)</translation>
+        <translation>Quitter le jeux (Echap)</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="115"/>
         <source>Offline help</source>
-        <translation type="finished">Aide hors-ligne</translation>
+        <translation>Aide hors-ligne</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="120"/>
         <source>View the game instructions (H)</source>
-        <translation type="finished">Voir les règles du jeux (H)</translation>
+        <translation>Voir les règles du jeux (H)</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="127"/>
         <source>Online help</source>
-        <translation type="finished">Aide en ligne</translation>
+        <translation>Aide en ligne</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="131"/>
         <source>Visit the game&apos;s website</source>
-        <translation type="finished">Visiter le site web</translation>
+        <translation>Visiter le site web</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="214"/>
         <source>Select a game type: </source>
-        <translation type="finished">Selection du type de jeux: </translation>
+        <translation>Selection du type de jeux: </translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="232"/>
@@ -91,7 +91,7 @@
     <message>
         <location filename="../optionsgamestate.cpp" line="313"/>
         <source>Next</source>
-        <translation type="finished">Suivant</translation>
+        <translation>Suivant</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="318"/>
@@ -101,12 +101,12 @@
     <message>
         <location filename="../optionsgamestate.cpp" line="329"/>
         <source>Cancel</source>
-        <translation type="finished">Annuler</translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="../optionsgamestate.cpp" line="334"/>
         <source>Return to main menu (Escape)</source>
-        <translation type="finished">Retourner au menu principale (Echap)</translation>
+        <translation>Retourner au menu principale (Echap)</translation>
     </message>
 </context>
 </TS>
