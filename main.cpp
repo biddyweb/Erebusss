d1a5bc3e76ae6b9aa71a9bc644352a683a4dd632
Mark Harman <mark.harman@ntlworld.com>
2013-06-06 23:45:17 +0100

    Fix for translations on Android.

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     //locale = QLocale(QLocale::French, QLocale::France).name().section('_', 0, 0); // test
     qDebug("locale: %s", locale.toStdString().c_str());
     QTranslator translator;
-    if( !translator.load(QString("ts/erebus_") + locale) ) {
+    if( !translator.load(DEPLOYMENT_PATH + QString("ts/erebus_") + locale) ) {
         qDebug("failed to load translator file");
     }
     app.installTranslator(&translator);
